648cce94831d8cb185782cec67dc914d9e5299f8
jeanette sofie bundgaard madsen <jean9415@stud.kea.dk>
2019-02-26 15:58:48 +0100

    prepared functions for sorting methodes

--- studentlist.js
+++ studentlist.js
@@ -2,11 +2,12 @@
 
 window.addEventListener("DOMContentLoaded", init);
 
-// global variabel of our student
+// global variabel of our student prototype
 let Student = {
   firstname: "-student-firstname-",
   lastname: "-student-lastname-",
-  house: "-house-"
+  house: "-house-",
+  imagename: "-imagename-"
 };
 
 const allStudents = [];
@@ -16,7 +17,7 @@ function init() {
 
   loadJSON();
 
-  // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
+  // TODO: build list, add event listeners, show modal, find images, and other stuff ...
 }
 
 function loadJSON() {
@@ -29,20 +30,26 @@ function loadJSON() {
     });
 }
 
+// the DATA part of the code
+
 function prepareObjects(jsonData) {
   jsonData.forEach(jsonObject => {
     // create a new student object from our student variable
     const student = Object.create(Student);
 
-    //the data from our Json-file we want to split.
+    //the data from our Json-file that we want to split.
     const parts = jsonObject.fullname.split(" ");
 
-    // how we want our data to be split:
+    // how we want the data to be displayed:
     student.firstname = parts[0];
-    student.lastname = parts[2];
+    // to get ONLY the last name, we should have the length of the parts -1
+    student.lastname = parts[parts.length - 1];
 
     // this should be accessed directly from the json-file and not split like the fullname
     student.house = jsonObject.house;
+
+    // TODO: her skal der indsættes img'er ved hjælp af uppercase/ lowercase, imagename er tilføjet
+    // vores prototype under de globale variabler
 
     // this stores all our students in a global array
     allStudents.push(student);
@@ -59,7 +66,44 @@ function filterList() {
 function sortList(filteredList) {
   console.log("sortList");
 
+  // TODO: her skal functionskalde med if statments der fortæller
+  // noget med at vores string er === så somthing-somthing ellers ..
+  // compareFirstname();
+  // compareLastname();
+  //compareHouse();
   displayList(filteredList);
+}
+
+function compareFirstname(a, b) {
+  // TODO:
+  // if (a.firstname < b.firstname){
+  // return -1;
+  // } else {
+  //  return 1;
+  // }
+  console.log("compareFirstname");
+}
+
+function compareLastname() {
+  console.log("compareLastname");
+
+  // TODO:
+  // if (a.lastname < b.lastname){
+  // return -1;
+  // } else {
+  //  return 1;
+  // }
+}
+
+function compareHouse() {
+  console.log("compareHouse");
+
+  // TODO:
+  // if (a.house < b.house){
+  // return -1;
+  // } else {
+  //  return 1;
+  // }
 }
 
 function displayList(student) {
@@ -70,17 +114,21 @@ function displayList(student) {
   student.forEach(displayStudent);
 }
 
+// the VISUEL part of the code
+
 function displayStudent(student) {
   // creates a clone from our student
-  let clone = document
+  let studentclone = document
     .querySelector("template#student")
     .content.cloneNode(true);
 
   // this sets the data for the clone
-  clone.querySelector("[data-field=firstname]").textContent = student.firstname;
-  clone.querySelector("[data-field=lastname]").textContent = student.lastname;
-  clone.querySelector("[data-field= house]").textContent = student.house;
+  studentclone.querySelector("[data-field=firstname]").textContent =
+    student.firstname;
+  studentclone.querySelector("[data-field=lastname]").textContent =
+    student.lastname;
+  studentclone.querySelector("[data-field= house]").textContent = student.house;
 
   // then oure clone is appendet to our list
-  document.querySelector("#list tbody").appendChild(clone);
+  document.querySelector("#list tbody").appendChild(studentclone);
 }
